8b9c686e850d953717947b8a51a406b565a5b56d
jedi <amajid@nodeflux.io>
2020-05-01 21:56:41 +0700

    move assets

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -3,18 +3,18 @@ from twtgen.utils import read_wikidump, count_vocab
 from spacy.lang.id import stop_words
 
 def test_read_wikidump_medium():
-    wikidump=read_wikidump(txt_file='tests/assets/dummy_medium')
+    wikidump=read_wikidump(txt_file='tests/assets/wikidump/medium')
     titles = ["Asam deoksiribonukleat", "Anwar Sadat", "Azhar Mansor"]
     for (idx, (k, _)) in enumerate(wikidump.items()):
         assert titles[idx] == k
 
 def test_read_wikidump_small_1():
-    wikidump=read_wikidump(txt_file='tests/assets/dummy_small_1')
+    wikidump=read_wikidump(txt_file='tests/assets/wikidump/small_1')
     assert wikidump['Boy']=='Namaku "Boy". Aku adalah anak tunggal'
     assert wikidump['BIN']=="kijang satu, kijang dua, ganti !!!"
 
 def test_read_wikidump_small_2():
-    wikidump=read_wikidump(txt_file='tests/assets/dummy_small_2')
+    wikidump=read_wikidump(txt_file='tests/assets/wikidump/small_2')
     assert wikidump['Peradaban']=="""Lagu Peradaban jauh lebih keras dari lagu metal apapun yang pernah kami dengar, geramnya sampai kebas."""
     assert wikidump['pergijauh vs wordfangs']=="""Kalo lu denger rock terus bertahun-tahun lo akan bosen gitu. Sekarang anak-anak udah gak denger rock lagiGue masih denger sih"""
 
